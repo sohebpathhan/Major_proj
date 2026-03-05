277c1bccb874076a9fd7b03b18e5747e99ba3887
sohebpathhan <shohebpathan12344@gmai.com>
2026-03-06 02:52:13 +0530

    2stagest added

--- flight-reservation-app-main/FlightReservationApplication/backend.groovy
+++ flight-reservation-app-main/FlightReservationApplication/backend.groovy
@@ -14,5 +14,35 @@ pipeline{
                 '''
             }
         }
+        stage('QA_TEst'){
+            steps{
+               withSonarQubeEnv(installationName: 'Sonar', credentialsId: 'Sonar_cred') {
+                      sh '''
+                      cd flight-reservation-app-main\FlightReservationApplication
+                      mvn sonar:sonar -Dsonar.projectKey=flight-reservation
+                       '''
+
+                      }
+            }
+        }
+
+         stage('Docker-Build'){
+            steps{
+                sh '''
+                cd flight-reservation-app-main\FlightReservationApplication
+                docker build -t sohebpathhan/flight_resor:latest .
+                docker push sohebpathhan/flight_resor:latest
+                docker rmi sohebpathhan/flight_resor:latest
+                '''
+            }
+
     }
+     stage('Deploy_stage'){
+            steps{
+                sh '''
+               cd flight-reservation-app-main\FlightReservationApplication
+               kubectl apply -f k8s/
+               '''
+            }
+        }
 }
