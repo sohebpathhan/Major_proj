97b490ed40e52e62a3c7b046917d6b73935f5856
sohebpathhan <shohebpathan12344@gmai.com>
2026-03-06 01:13:55 +0530

    build stage added

--- flight-reservation-app-main/FlightReservationApplication/backend.groovy
+++ flight-reservation-app-main/FlightReservationApplication/backend.groovy
@@ -6,5 +6,13 @@ pipeline{
                 git branch: 'main', url: 'https://github.com/sohebpathhan/Major_proj.git'
             }
         }
+          stage('code-Build'){
+            steps{
+                sh '''
+                cd flight-reservation-app-main\FlightReservationApplication
+                mvn clean package
+                '''
+            }
+        }
     }
 }
