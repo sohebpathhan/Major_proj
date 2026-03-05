fe65670aa5bbd930ff0607b9c57487d9613f04e6
sohebpathhan <shohebpathan12344@gmai.com>
2026-03-06 00:53:26 +0530

    pipeline 1st

--- flight-reservation-app-main/FlightReservationApplication/backend.groovy
+++ flight-reservation-app-main/FlightReservationApplication/backend.groovy
@@ -1,51 +1,9 @@
 pipeline{
     agent any
     stages{
-        stage('Code-pull'){
+        stage('code-pull'){
             steps{
-                git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'
-            }
-
-        }
-        stage('Code-Build'){
-            steps{
-                sh '''
-                    cd FlightReservationApplication
-                    mvn clean package
-                '''
-            }
-
-        }
-        stage('Code-Test'){
-            steps{
-               withSonarQubeEnv(installationName: 'Sonar', credentialsId: 'sonar-token') {
-                 sh '''
-                    cd FlightReservationApplication
-                    mvn sonar:sonar -Dsonar.projectKey=flight-reservation
-                 '''
-                }
-            }
-
-        }
-        stage('Docker-Build'){
-            steps{
-                sh '''
-                    cd FlightReservationApplication
-                    docker build -t mayurwagh/flightreservation-pls17:latest .
-                    docker push mayurwagh/flightreservation-pls17:latest
-                    docker rmi mayurwagh/flightreservation-pls17:latest
-
-                '''
-                
-            }
-
-        }
-        stage('Deploy'){
-            steps{
-                sh '''
-                    cd FlightReservationApplication
-                    kubectl apply -f k8s/
-                '''
+                git branch: 'main', url: 'https://github.com/sohebpathhan/Major_proj.git'
             }
         }
     }
